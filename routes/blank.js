636ad3e844cbdb0b6519ce6e30395f2897604f5b
itssuyog96 <itssuyog96@gmail.com>
2016-09-14 02:14:45 +0530

    added assets

--- routes/blank.js
+++ routes/blank.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/blank', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('blank');
 });
 
