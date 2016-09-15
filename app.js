401b74c42e79b6055d6dc0a3c2d9ba300fcf7154
adi-sk <adikrhd@gmail.com>
2016-09-16 01:30:10 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/blank', blank);
 app.use('/admin', admin);
+app.get('/select', function(req, res, next) {
+
+  var data={
+    colg_id:123,
+    clog_name:'Aditya'
+  };
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(data);
+  res.end();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
