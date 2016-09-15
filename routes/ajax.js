8bf99a021ad1bed3a22e53dae2cc304605370d45
adi-sk <adikrhd@gmail.com>
2016-09-16 02:37:30 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app.js

---
+++ routes/ajax.js
@@ -0,0 +1,82 @@
+/**
+ * Created by adikr on 16-09-2016.
+ */
+
+var db =require('../db-config');
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/load_colg', function(req, res, next) {
+
+    db.query('SELECT * FROM `college`',function (err,result) {
+
+        console.log(result);
+        if (err) throw err;
+        else
+        {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(result));
+            res.end();
+        }
+    });
+});
+
+
+
+
+router.get('/load_dept', function(req, res, next) {
+
+    db.query('SELECT * FROM `department`',function (err,result) {
+
+        console.log(result);
+        if (err) throw err;
+        else
+        {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(result));
+            res.end();
+        }
+    });
+});
+
+
+
+
+router.get('/load_prof', function(req, res, next) {
+
+    db.query('SELECT * FROM `professor`',function (err,result) {
+
+        console.log(result);
+        if (err) throw err;
+        else
+        {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(result));
+            res.end();
+        }
+    });
+
+});
+
+
+router.get('/load_sub', function(req, res, next) {
+
+    db.query('SELECT * FROM `subject`',function (err,result) {
+
+        console.log(result);
+        if (err) throw err;
+        else
+        {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(result));
+            res.end();
+        }
+    });
+
+});
+
+
+
+module.exports = router;
---
+++ routes/survey.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Omkar Dubas on 15-09-2016.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('survey');
+
+});
+
+module.exports = router;
