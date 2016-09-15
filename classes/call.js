64c338c0089d18d39fa03e53c55c8dcfbe48d76b
adi-sk <adikrhd@gmail.com>
2016-09-15 19:44:33 +0530

    call.js

---
+++ classes/call.js
@@ -0,0 +1,15 @@
+/**
+ * Created by adikr on 15-09-2016.
+ */
+var prof= require('./prof');
+
+var p= new prof();
+var info={
+    col_id:123,
+    dept_id:567,
+    prof_id:890,
+    prof_name:'Omkar'
+};
+
+
+p.set(info);
