fe41b56b309fb3f1da77a91c13fcf6ebef9067ce
itssuyog96 <itssuyog96@gmail.com>
2016-09-16 02:18:00 +0530

    admin frontend scripts update 1

--- routes/survey.js
+++ routes/survey.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('survey');
+    res.render('survey', {subject : ["SUB1", "SUB2", "SUB3"]});
 
 });
 
