36458709f97da962ebaf46d4e783ac169f604573
adi-sk <adikrhd@gmail.com>
2016-10-02 22:24:03 +0530

    admin redirect

--- routes/settings.js
+++ routes/settings.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('settings',{name:req.user.username, pass:req.user.password});
+    res.render('settings');
 
 });
 
