2f9ca4576a1382415ef80fd1e135cdc8f3f7c91d
Omkar Dubas <chandandubas@gmail.com>
2016-11-02 19:20:58 +0530

    graph

--- routes/graphsurvey.js
+++ routes/graphsurvey.js
@@ -4,10 +4,28 @@
 
 var express = require('express');
 var router = express.Router();
+var tiles =  require('../dashmeta.json').feeder;
+var table = require('../tablemeta.json').other;
+var menu = require('../menu.json').feed_analyzer;
+var reports = require('../reports.json').topics;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('graphsurvey');
+
+    if(req.session.login) {
+        if(req.session.passport.user.role == 'feed_analyzer'){
+            res.render('graphsurvey', {dash : tiles, menu : menu, user: req.session.passport.user,  profs : table.professor, rating: reports.rating});
+        }
+        else{
+            delete req.session.redirectTo;
+            res.redirect('/graphsurvey');
+        }
+    }
+    else {
+        req.session.redirectTo = '/feed_analyzer/';
+        res.redirect('/graphsurvey');
+    }
+
 
 });
 
