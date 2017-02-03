0325285da70b45fc88ea47f8ad7dede6eb743619
Suyog Gadhave <itssuyog96@gmail.com>
2017-02-03 16:30:35 +0530

    Professor entry / deletion done                            Login frontend enhancement                            Settings page frontend done

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var tdata = require('../tablemeta.json').admin;
 var tiles = require('../dashmeta.json').admin;
 var mdata = require('../modalmeta.json');
-var menu = require('../menu.json').feed_analyzer;
+var menu = require('../menu.json').admin;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -45,6 +45,23 @@ router.get('/manage', function(req, res, next){
         res.redirect('/login');
     }
 
+});
+
+router.get('/manage_login', function(req, res, next){
+    if(req.session.login) {
+        if(req.session.passport.user.role == 'admin'){
+            res.render('manage_login', {menu : menu, user : req.session.passport.user});
+        }
+        else{
+            delete req.session.redirectTo;
+            res.redirect('/login');
+        }
+
+    }
+    else {
+        req.session.redirectTo = '/admin/manage';
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
